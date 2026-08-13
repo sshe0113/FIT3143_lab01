f2829bf82ddc16012a59c2b1e35691379107bc7a
XCZ1007 <sengcheng.shee@icloud.com>
2026-08-13 15:55:45 +0800

    Add header

---
+++ task1.c
@@ -0,0 +1,12 @@
+//////////////////////////////////////
+//  Author  :   Shee Seng Cheng
+//              Tay Chee Hsian
+//  Start_dt:   13/08/2026
+//////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <time.h>
+#include <string.h>
+#include <omp.h>
